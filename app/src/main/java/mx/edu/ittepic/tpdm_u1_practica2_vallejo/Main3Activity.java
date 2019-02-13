f571e25216d3b7912c0ed0371fb8e2e8a006ea53
Morneff <wibrvallejoma@gmail.com>
2019-02-12 21:20:39 -0700

    Adicion de archivos

---
+++ app/src/main/java/mx/edu/ittepic/tpdm_u1_practica2_vallejo/Main3Activity.java
@@ -0,0 +1,13 @@
+package mx.edu.ittepic.tpdm_u1_practica2_vallejo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
